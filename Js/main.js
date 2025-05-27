40b7b0b218ef4cfdd98557a49abff2416ca21cdd
coluccifrancesco <coluccif2002@gmail.com>
2025-05-27 15:33:10 +0200

    Update JavaScript logic for train ticket pricing and add validation checks as Bonus

--- Js/main.js
+++ Js/main.js
@@ -20,24 +20,32 @@ let elderDiscount = costPerKm /100 * 60;
 
 if (userAge < 18) {
 
-    console.log(youngDiscount)
+    console.log(youngDiscount + " " + "€")
 
 } else if (userAge >= 65) {
 
-    console.log(elderDiscount)
+    console.log(elderDiscount  + " " + "€")
 
 } else {
 
-    console.log(costPerKm)
+    console.log(costPerKm  + " " + "€")
 
 }
 
 //   L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-    
-
 
 /*    
     TIP: per controllare che la vostra logica sui prezzi funzioni correttamente, provate a verificare quanto segue:
     100km, 10 anni => prezzo corretto: €16.80
     100km, 70 anni => prezzo corretto: €12.60
 */
+
+if (userKm == 100 && userAge == 10 && youngDiscount == 16.8) {
+    
+    console.log("Funziona!")
+
+}  else if (userKm == 100 && userAge == 65 && elderDiscount == 12.6) {
+    
+    console.log("Funziona!")
+
+}
